df17d1964d8775375f555e9121fcdcf1337ab0e8
Drugov Viktor <engistau@gmail.com>
2021-03-05 18:07:08 +0600

    .Create

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,69 @@
+package client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+	private final Socket socket;
+	private final DataInputStream in;
+	private final DataOutputStream out;
+
+	public Client() throws IOException {
+		socket = new Socket("localhost", 1235);
+		in = new DataInputStream(socket.getInputStream());
+		out = new DataOutputStream(socket.getOutputStream());
+		runClient();
+	}
+
+	private void runClient() {
+		JFrame frame = new JFrame("Cloud Storage");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+
+		JTextArea ta = new JTextArea();
+		// TODO: 02.03.2021
+		// list file - JList
+		JButton uploadButton = new JButton("Upload");
+
+		frame.getContentPane().add(BorderLayout.CENTER, ta);
+		frame.getContentPane().add(BorderLayout.SOUTH, uploadButton);
+
+		frame.setVisible(true);
+
+		uploadButton.addActionListener(a -> {
+			System.out.println(sendFile(ta.getText()));
+		});
+	}
+
+	private String sendFile(String filename) {
+		try {
+			File file = new File("client" + File.separator + filename);
+			if (file.exists()) {
+				out.writeUTF("upload");
+				out.writeUTF(filename);
+				long length = file.length();
+				out.writeLong(length);
+				FileInputStream fis = new FileInputStream(file);
+				int read = 0;
+				byte[] buffer = new byte[256];
+				while ((read = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, read);
+				}
+				out.flush();
+				String status = in.readUTF();
+				return status;
+			} else {
+				return "File is not exists";
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Something error";
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Client();
+	}
+}
